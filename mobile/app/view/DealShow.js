6454d2f1d87db40af2d6291797dfbf2cbcfabf65
Kasper Pihl Tornøe <kasper@pihl.it>
2012-03-05 21:43:43 +0100

    Sencha skull køre bedre nu

--- mobile/app/view/DealShow.js
+++ mobile/app/view/DealShow.js
@@ -3,30 +3,15 @@ Ext.define('QuickD.view.DealShow', {
     xtype: 'dealshow',
 	
     config: {
-        items: [
-            {
-                id: 'content',
-                tpl: [
-                    '<div class="x-contacts top">',
-                        '<div id="deal_{id}">',
-                            '<div class="headshot" style="background-image:url(\''+IMG_URL+'thumbnail/{image}\');"></div>',
-                                '<h1>{title}</h1>',
-                                '<div class="prices">',
-                                priceIt('{orig_price}'),
-                            '</div>',
-                            '<div class="footer"><span class="shop">{name}</span></div>',
-                        '</div>',
-                    '</div>'
-                ].join('')
-            }
-        ],
-
+        tpl: '<div>2{title}</div>',
+        data: {title:'test'},
         record: null
     },
 
     updateRecord: function(newRecord) {
+        log(newRecord);
       if (newRecord) {
-            this.down('#content').setData(newRecord.data);
+            this.setData(newRecord.data);
          }
          
     }
--- mobile/app/view/Main.js
+++ mobile/app/view/Main.js
@@ -8,7 +8,43 @@ Ext.define('QuickD.view.Main', {
     ],
     config:{
         fullscreen: 'true',
-        items: [{xtype:'deallist'}]
+        layout: 'card',
+        items: [
+        {
+            ui:'sencha',
+            id: 'topbar',
+            hidden: true,
+            xtype : 'toolbar',
+            docked: 'top',
+            items:[
+            {
+                xtype:'button',
+                ui:'back',
+                text:'Tilbage'
+            },
+            {
+                xtype:'spacer'
+            },
+            {
+                xtype:'selectfield',
+                id:'filterButton',
+                width: '150px',
+                options: [
+                    { text: 'Alle deals',  value: 'all' },
+                    { text: 'Mad & Drikke', value: 'fooddrink' },
+                    { text: 'Shopping', value: 'shopping' },
+                    { text: 'Oplevelser', value: 'experience' }
+                ],
+                align:'left'
+            }]
+        },
+        {
+            xtype:'panel',
+            html: 'splash'
+        },
+        {xtype:'deallist'},
+        {xtype:'dealshow'}
+        ]
     },
     applyLayout: function(config) {
       config = config || {};
