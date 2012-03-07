2916c010483c78b9e4506dc999651d870788a58f
Jeppe <jstougaard@gmail.com>
2012-03-07 18:35:55 +0100

    FB login progress

--- brugerinfo/js/script.js
+++ brugerinfo/js/script.js
@@ -17,11 +17,52 @@ $(function() {
         */
         event.preventDefault();
     });
+
+    
+    
+    
+    
+
+   
+
 });
 
 
+//Part for Facebook signup
+(function(d){
+    var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    d.getElementsByTagName('head')[0].appendChild(js);
 
 
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '286675801401479',
+        status     : true, 
+        cookie     : true,
+        xfbml      : true,
+        oauth      : true,
+      });
+    };
+
+    //Register button click
+    $('#btn_fb_signup').click(function() { doFBSubscribe();  });
+}(document));
+
+ function doFBSubscribe() {
+    FB.login(function(response) {
+      if (response.authResponse) {
+        FB.api('/me', function(response) {
+            console.log("Facebook login");
+            console.log(response);
+          //X.get(WEB_ROOT +"/ajax/subscribe.php?action=do_fb_subscribe&email=" + response.email + "&real_name=" + response.name + "&gender=" + response.gender + "&username=" + response.username + "&id=" + response.id);
+        });
+      } else {
+
+      }
+    }, {scope: 'email'});
+}
 
 
 
