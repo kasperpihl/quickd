44f401ed0e26d755415737070290271d4a37b385
Kasper Pihl Tornøe <kasper@pihl.it>
2012-03-08 23:42:14 +0100

    test 5 auto updating

--- dealer/test.php
+++ dealer/test.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../config.php');
-print_r('test4');
+print_r('test5');
 /*$result = array();
 $array = array('Denis','Anders','Kasper','Kristjan');
 for ($i = 0; $i < 10000 ; $i++){
