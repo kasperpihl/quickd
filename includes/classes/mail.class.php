5f7ec00ee3f0bc5c627ad4861cef4f67ba85f1e1
Kasper Pihl Tornøe <kasper@pihl.it>
2012-03-08 18:40:09 +0100

    Mail class fixed to make welcome message

--- includes/classes/mail.class.php
+++ includes/classes/mail.class.php
@@ -1,6 +1,26 @@
 <?php 
 Class Mail{
-	static function sendMail(){
-		
+	protected static $sender = 'mail@quickd.dk';
+	public static function sendBetaConfirmation($mail){
+		$subject = 'Registrering til beta-lancering af QuickD';
+		$message = 
+			'Tusind tak for din interesse i QuickD.\r\n\r\n'.
+			'Du er nu skrevet op til beta-testen i Aarhus og vi sender dig en invitation på åbningsdagen d. 20. april.\r\n\r\n'.
+			'Hvis du har nogle spørgsmål, er du meget velkommen til at besvare denne mail.\r\n\r\n'.
+			'Vi glæder os helt vildt til at kunne lancere for dig!\r\n\r\n'.
+			'De bedste hilsner,\r\n'.
+			'QuickD-teamet';
+		self::sendMail($mail,$subject,$message);
+	}
+	private static function sendMail($to,$subject,$message){
+		$headers = 
+			'From: QuickD-teamet ' .self::$sender . "\r\n" . 
+			'Reply-To: '.self::$sender . "\r\n" .
+    		'X-Mailer: PHP/' . phpversion();
+		mail($to,$subject,$message,$headers);
 	}
 }
+
+Mail::sendBetaConfirmation('kasper@tornoe.org');
+Mail::sendBetaConfirmation('kasperpihl@me.com');
+?>
