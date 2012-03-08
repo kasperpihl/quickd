c25adcd8aac09b1b82d0447dfa28522c3130f6ca
Kasper Pihl Tornøe <kasper@pihl.it>
2012-03-08 23:34:04 +0100

    Ja tak commit dem nu

--- dealer/test.php
+++ dealer/test.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../config.php');
-print_r('test');
+print_r('test2');
 /*$result = array();
 $array = array('Denis','Anders','Kasper','Kristjan');
 for ($i = 0; $i < 10000 ; $i++){
