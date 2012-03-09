be681e57ea2ec72cef10b9ec8d365bc189625229
Kasper Pihl Tornøe <kasper@pihl.it>
2012-03-09 20:10:58 +0100

    Putted the couch updater under admin for more security :-)

--- admin/couch/delete.php
+++ admin/couch/delete.php
@@ -1,4 +1,5 @@
 <?php require_once('../config.php');
+require_admin();
 $array = $db->getView('dealer','cleanDeals');
 $ret = array();
 foreach($array->rows as $item){
---
+++ admin/couch/update.php
@@ -0,0 +1,7 @@
+<?php require_once('../config.php'); 
+require_admin();
+require_once('design_admin.php');
+require_once('design_dealer.php');
+require_once('design_quickd.php');
+require_once('configuration.php');
+?>
