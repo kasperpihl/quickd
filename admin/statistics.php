39f40d003e669aa00d04a0669ceca10daa2e2f6f
Kasper Pihl Tornøe <kasper@pihl.it>
2012-03-09 20:30:11 +0100

    fixed statistik til ikke at inkludere admins.

--- admin/statistics.php
+++ admin/statistics.php
@@ -3,8 +3,11 @@ require_once('../config.php');
 require_admin();
 $users = $db->getView('dealer','getUsersByMail');
 $users = $users->rows;
-echo 'Registrerede brugere: '.sizeof($users).'<br/>';
+
+$users = array_filter($users,function($user){ return ($user->value->privileges == 1); });
 $facebookUsers = array_filter($users,function($user){ return isset($user->value->fb_info); });
+
+echo 'Registrerede brugere: '.sizeof($users).'<br/>';
 echo 'Registreret med facebook: '.sizeOf($facebookUsers).'<br/><br/>';
 if(!empty($users)){
 	echo '<table><thead><th>Email</th><th>Facebook</th></thead><tbody>';
