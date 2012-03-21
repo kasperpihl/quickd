35600de74f3f5ea15c356eb69308fde91b1e283c
Kasper Pihl Tornøe <kasper@pihl.it>
2012-03-21 14:30:11 +0100

    Added name to Reply to header

--- includes/classes/mail.class.php
+++ includes/classes/mail.class.php
@@ -33,7 +33,7 @@ Class Mail{
 		$headers = 
 			'MIME-Version: 1.0' ."\n".
 			'From: QuickD-teamet <' .self::$sender . ">". "\n" . 
-			'Reply-To: '.self::$sender . "\n" .
+			'Reply-To: QuickD-teamet <'.self::$sender . ">\n" .
 			'Content-Type: text/plain; charset=utf-8'."\n".
     		'X-Mailer: PHP/' . phpversion();
 		mail($to,$subject,$message,$headers);
