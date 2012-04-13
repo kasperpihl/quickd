ce198ce85867da94e1c21db7d66b4e38e74d8daf
Jens Ahrengot Boddum <ahrengot@gmail.com>
2012-04-14 01:38:33 +0200

    removed web app meta tags again as they're already being generated by Sencha

--- mobile/index.php
+++ mobile/index.php
@@ -3,8 +3,6 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 
         <title>QuickD</title>
 	<!--<script type="text/javascript" src="http://maps.google.com/maps/api/js?v=3&sensor=true"></script>
