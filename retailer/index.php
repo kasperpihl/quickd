c0e8412fb08926e3c4b953bcf03c62b1cf9c3f41
Kasper Pihl Tornøe <kasper@pihl.it>
2012-04-14 13:43:38 +0200

    Fixed logout for dealer mobile

--- retailer/index.php
+++ retailer/index.php
@@ -1,9 +1,11 @@
 <?php
 require_once('../config.php');
 define('COMMON_URL',REAL_URL.'common/');
+
 if(isset($_GET['logout'])){
 	$session->logout();
 	redirect(REAL_URL);
+	exit;
 }
 if($version == 'mobile')  require_once('mobile/index.php');
 else require_once('desktop/index.php');
