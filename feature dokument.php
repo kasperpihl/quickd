8129a9edc7338efed13fcf268dc4748c9ebc0be1
Kasper Pihl Tornøe <kasper@pihl.it>
2012-04-16 09:49:14 +0200

    Added a feature document

---
+++ feature dokument.php
@@ -0,0 +1,5 @@
+<h1>Feature dokument, ting vi gerne vil have lavet før eller siden.</h1>
+
+<p> Deal kalender (nem oversigt over fremtidige deals og hvornår de kører)
+
+<p> Avanceret start deal, så man kan vælge f.eks. hver tirsdag, 2 gange om ugen osv osv
