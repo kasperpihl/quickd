1bf3ef2645bcbb4e4f7a0f6ccb1295bf7d06a560
Kasper Pihl Tornøe <kasper@pihl.it>
2012-04-20 01:37:54 +0200

    Design document update

--- admin/couch/design_quickd.php
+++ admin/couch/design_quickd.php
@@ -25,7 +25,9 @@ try{
 	"function(doc){
 		if(!doc.hasOwnProperty('user')) return;
 		if(!doc.user.hasOwnProperty('userbeta')) return;
-		emit(doc.user.userbeta,doc._id);
+		var obj = {id:doc._id,email:doc.user.email};
+		if(doc.user.hasOwnProperty('sendmail') && doc.user.sendmail) obj.sendmail = true;
+		emit(doc.user.userbeta,obj);
 	}");
 	$views->getDeals = array("map"=>$getDeals);
 	echo 'views objektet klar <br/>';
@@ -42,6 +44,13 @@ try{
 		if(!query.hasOwnProperty('userbeta')) return [null,'userbeta_must_be_specified'];
 		doc.user.userbeta = query.userbeta;
 		return[doc,'success'];
+	}";
+	$updates->updateUser = 
+	"function(doc,req){
+		if(!req.query.hasOwnProperty('json')) return [null,'json_must_be_specified'];
+		var query = JSON.parse(req.query.json);
+		if(query.hasOwnProperty('sendmail')) doc.user.sendmail = query.sendmail;
+		return [doc,'success'];
 	}";
 	$doc->updates = $updates;
 	echo 'Forlader _design/quickd';
