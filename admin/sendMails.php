593297e86d60194c919b8a2185b271e6495d742b
Jeppe <jstougaard@gmail.com>
2012-04-20 02:03:21 +0200

    mail once more!

--- admin/sendMails.php
+++ admin/sendMails.php
@@ -1,5 +1,5 @@
 <?php 
 require_once(dirname(__FILE__).'/config.php');
-Mail::create('randomMail', 'kasper@tornoe.org',array('name'=>'Mr. Nice Guy', 'number'=>$i));
+Mail::create('randomMail', 'kasper@tornoe.org',array('name'=>'Mr. Nice Guy', 'number'=>'1337'));
 Mail::dequeueMails();
 ?>
