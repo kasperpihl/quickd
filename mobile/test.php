4cf3b9d9215fa3c3e2bea9f04f40fcfbfe47cfc7
Kasper Pihl Tornøe <kasper@pihl.it>
2012-04-20 07:39:56 +0200

    Testing script fixed hopefully

--- mobile/test.php
+++ mobile/test.php
@@ -5,10 +5,10 @@ require_once(dirname(__FILE__).'/../config.php');
 	Log::add($i);
 	sleep(1);
 }*/
-Log::printLog();
+//Log::printLog();
 echo "<br><br>";
 
-if ($_GET['spam']&&false) {
+if ($_GET['spam']) {
 	for ($i=0;$i<=200;$i++) {
 		Mail::create('sendInvite', 'ahrengot@gmail.com', array('betakey'=>b3df4ee1, 'name'=>'Fissekarl'));
 		//Mail::create('randomMail', $_GET['spam'],array('name'=>'Mr. Nice Guy', 'number'=>$i));
