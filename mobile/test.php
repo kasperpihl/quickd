947e86d5a80d28826b0c64de2c4bc9006a2894bd
Kasper Pihl Tornøe <kasper@pihl.it>
2012-04-20 13:00:48 +0200

    Testing mailing queue

--- mobile/test.php
+++ mobile/test.php
@@ -9,8 +9,8 @@ require_once(dirname(__FILE__).'/../config.php');
 echo "<br><br>";
 
 if ($_GET['spam']) {
-	for ($i=0;$i<=200;$i++) {
-		Mail::create('sendInvite', 'ahrengot@gmail.com', array('betakey'=>b3df4ee1, 'name'=>'Fissekarl'));
+	for ($i=0;$i<=5;$i++) {
+		Mail::create('sendInvite', 'kasper@tornoe.org', array('betakey'=>b3df4ee1, 'name'=>'Fissekarl'));
 		//Mail::create('randomMail', $_GET['spam'],array('name'=>'Mr. Nice Guy', 'number'=>$i));
 		echo "Created ".$i."<br />";
 	}
