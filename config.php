bbc1f300cb81aa0f4796b9d4a334fd332ab30eb9
Kasper Pihl Tornøe <kasper@pihl.it>
2012-05-17 00:25:02 +0200

    fixed error

--- config.php
+++ config.php
@@ -56,7 +56,7 @@ switch($root){
 		$end = '/'.$ending;
 		$histRoot = '/';
 		$cdnUrl = $_SERVER['SERVER_ADDR'].'/';
-		define('API_URL','http://'.$root.$end.'1/');
+		define('API_URL','http://'.$root.$histRoot.'1/');
 	break; 
 }
 $historyObj = json_encode(array('pushState'=>true,'root'=>$histRoot));
