a8a9e9c54ad3b79bfdaeae27260ca1c9608c78a7
Kasper Pihl Tornøe <kasper@pihl.it>
2012-05-23 17:09:44 +0200

    fixed countdown problem of weird file

--- feature dokument.php
+++ feature dokument.php
@@ -5,3 +5,11 @@
 <p> Avanceret start deal, så man kan vælge f.eks. hver tirsdag, 2 gange om ugen osv osv
 
 <p>Badge til at sætte på ved steder der f.eks har opnået x antal gode anmeldelser ....
+
+
+Slap
+2 retters menu
+Tirsdag og onsdag 17-21
+
+3 retters
+Lørdag 17-20
