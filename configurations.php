354005dc716a187f020b5da07e457e92d9ed0329
Kasper Pihl Tornøe <kasper@pihl.it>
2012-05-23 14:29:48 +0200

    added couch class

---
+++ configurations.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_USER','quickd');
+define('DB_PASS','ka2jae2n');
+define('DB_URL','localhost');
+define('TEST_DB_USER','quickd');
+define('TEST_DB_PASS','testanders');
+define('TEST_DB_URL','77.66.53.58');
+?>
