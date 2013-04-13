85d3d9e9061094e5f51e5c26de190436ce8cbb95
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-04-13 17:30:56 +0200

    Moved scripts to the bottom of index so the admin loads faster

--- retailer/desktop/index.php
+++ retailer/desktop/index.php
@@ -24,11 +24,6 @@ if(strpos($_SERVER['REQUEST_URI'], $thisfolder )){
 			  <link href="<?= ROOT_URL ?>styles/stylesheets/ie.css" media="all" rel="stylesheet"/>
 	    <![endif]-->
 	     
-	    <script src="<?= LIBS_URL ?>jquery/jquery-min.js"></script>
-		<script src="<?= LIBS_URL ?>underscore/underscore-min.js"></script>
-	    <script src="<?= LIBS_URL ?>backbone/backbone-min.js"></script>
-	    <script src="<?= ROOT_URL ?>js/function.js"></script>
-	    <script src="<?= ROOT_URL ?>js/lang.js"></script>
 	    <script> var shopowner; var debug = false;	
 			var ROOT_URL = "<?= ROOT_URL ?>";
 			var API_URL = "<?= API_URL ?>";
@@ -48,42 +43,49 @@ if(strpos($_SERVER['REQUEST_URI'], $thisfolder )){
 	
 	<body onLoad="resizeBg()">
     <div id="content"><!--bg-test.jpg-->
-      <img id="bgImage" class="bgImage" style="display:none" src="<?= ROOT_URL ?>styles/stylesheets/i/bg-test.jpg" />
+      <img id="bgImage" class="bgImage" style="display:none;" src="<?= ROOT_URL ?>styles/stylesheets/i/bg-test.jpg" />
       <div id="dashboard"></div>
-		</div>
-			<!-- Main javascript files to get it all to work -->
-			
-			<!--[if lt IE 9]>
-			<script type="text/javascript">
-					$(function() {
-						var showModal = function() {
-							var msg = "Looks like you're using an outdated browser. \nOld browsers are insecure, slow and make websites behave wonky. \n \nLuckily it's free and easy to upgrade – Just swing by http://browsehappy.com/ Click ok to go there now. \n";
-							if (confirm(msg)) window.location.href = "http://browsehappy.com/";
-						}
-						
-						setTimeout(showModal, 1500);
-					});
-				</script>
-			<![endif]-->
+	</div>
+		    
+	    <script src="<?= LIBS_URL ?>jquery/jquery-min.js"></script>
+		<script src="<?= LIBS_URL ?>underscore/underscore-min.js"></script>
+	    <script src="<?= LIBS_URL ?>backbone/backbone-min.js"></script>
+	    <script src="<?= ROOT_URL ?>js/function.js"></script>
+	    <script src="<?= ROOT_URL ?>js/lang.js"></script>
+		
+		<!-- Main javascript files to get it all to work -->
+		
+		<!--[if lt IE 9]>
+		<script type="text/javascript">
+				$(function() {
+					var showModal = function() {
+						var msg = "Looks like you're using an outdated browser. \nOld browsers are insecure, slow and make websites behave wonky. \n \nLuckily it's free and easy to upgrade – Just swing by http://browsehappy.com/ Click ok to go there now. \n";
+						if (confirm(msg)) window.location.href = "http://browsehappy.com/";
+					}
+					
+					setTimeout(showModal, 1500);
+				});
+			</script>
+		<![endif]-->
 
-			<script src="<?= LIBS_URL ?>modernizr/modernizr-2.0.6.custom.js"></script>
-			<script src="<?= LIBS_URL ?>jquery/jquery-ui.js"></script>
-			<!--<script src="https://maps.googleapis.com/maps/api/js?v=3.5&sensor=false&region=DK"></script>-->
-	    	<script src="<?= LIBS_URL ?>jquery/jquery.ui.selectmenu.js"></script>
-	    	<script src="<?= LIBS_URL ?>jquery/jcarousellite.min.js"></script>
-	    	<script src="<?= LIBS_URL ?>jquery/jquery.slides.min.js"></script>        
-	    	<script src="<?= LIBS_URL ?>jquery/jquery.mousewheel.min.js" ></script>
-	    	<script src="<?= LIBS_URL ?>jquery/jquery.rotate.min.js" ></script>
-			<script src="<?= LIBS_URL ?>jquery/jquery.meow.js" ></script>
-			<script src="<?= LIBS_URL ?>jquery/jquery.easing.js"></script>
-			<script src="<?= LIBS_URL ?>jquery/jquery.tools.min.js"></script>
+		<script src="<?= LIBS_URL ?>modernizr/modernizr-2.0.6.custom.js"></script>
+		<script src="<?= LIBS_URL ?>jquery/jquery-ui.js"></script>
+		<!--<script src="https://maps.googleapis.com/maps/api/js?v=3.5&sensor=false&region=DK"></script>-->
+    	<script src="<?= LIBS_URL ?>jquery/jquery.ui.selectmenu.js"></script>
+    	<script src="<?= LIBS_URL ?>jquery/jcarousellite.min.js"></script>
+    	<script src="<?= LIBS_URL ?>jquery/jquery.slides.min.js"></script>        
+    	<script src="<?= LIBS_URL ?>jquery/jquery.mousewheel.min.js" ></script>
+    	<script src="<?= LIBS_URL ?>jquery/jquery.rotate.min.js" ></script>
+		<script src="<?= LIBS_URL ?>jquery/jquery.meow.js" ></script>
+		<script src="<?= LIBS_URL ?>jquery/jquery.easing.js"></script>
+		<script src="<?= LIBS_URL ?>jquery/jquery.tools.min.js"></script>
 
-			<script src="<?= LIBS_URL ?>jquery/browserDetect.js"></script>
-			<script src="<?= LIBS_URL ?>jquery/date.js"></script>
-			<script src="<?= LIBS_URL ?>jquery/jquery.validate.js"></script>
-			<script src="<?= LIBS_URL ?>jquery/jquery.tools.min.js"></script>
-			<!--<script src="animate.js"></script>-->
-			
-			<script src="<?= LIBS_URL ?>require/require.js" data-main="<?= ROOT_URL ?>js/main"></script>
+		<script src="<?= LIBS_URL ?>jquery/browserDetect.js"></script>
+		<script src="<?= LIBS_URL ?>jquery/date.js"></script>
+		<script src="<?= LIBS_URL ?>jquery/jquery.validate.js"></script>
+		<script src="<?= LIBS_URL ?>jquery/jquery.tools.min.js"></script>
+		<!--<script src="animate.js"></script>-->
+		
+		<script src="<?= LIBS_URL ?>require/require.js" data-main="<?= ROOT_URL ?>js/main"></script>
 	</body>
 </html>
