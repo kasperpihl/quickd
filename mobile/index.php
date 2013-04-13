f8be5f9b93c0f206990ab69b7afee9dbce19184a
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-04-13 16:43:46 +0200

    Clean up index.php for mobile and move blocking google maps api to the bottom for faster page loading

--- mobile/index.php
+++ mobile/index.php
@@ -6,80 +6,75 @@ $isIphone = (isset($_SESSION['iphone'])) ? $_SESSION['iphone'] : $uagent->Detect
 if(!isset($_SESSION['iphone'])) $_SESSION['iphone'] = $isIphone;
 $betakey = false;
 if(!isset($_SESSION['userbeta_email'])){
-    if(isset($_COOKIE['betakey'])) $betakey = $_COOKIE['betakey'];
-    else if(isset($_GET['betakey'])) $betakey = $_GET['betakey'];
+	if(isset($_COOKIE['betakey'])) $betakey = $_COOKIE['betakey'];
+	else if(isset($_GET['betakey'])) $betakey = $_GET['betakey'];
 
-    $user = validateBetakey($betakey);
-    if(isset($_GET['betakey']) && !$isIphone) redirect();
+	$user = validateBetakey($betakey);
+	if(isset($_GET['betakey']) && !$isIphone) redirect();
 }
 else $user = $_SESSION['userbeta_email'];
 $sendmail = (isset($_SESSION['sendmail']) && $_SESSION['sendmail']);
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-        <title>QuickD</title>
-        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
-        <!--Sencha Touch -->
-        <meta property="og:title" content="QuickD" />
-        <meta property="og:description" content="En ny måde at handle på - snart i din by."/>
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://www.quickd.dk" />
-        <meta property="og:image" content="<?=ROOT_URL?>resources/images/logo_medium.jpg" />
-        <meta property="og:site_name" content="QuickD" />
-        <meta property="fb:admins" content="508608046" />
+	<title>QuickD</title>
+	
+	<!--Sencha Touch -->
+	<meta property="og:title" content="QuickD" />
+	<meta property="og:description" content="En ny måde at handle på - snart i din by."/>
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="http://beta.quickd.com" />
+	<meta property="og:image" content="<?=ROOT_URL?>resources/images/logo_medium.jpg" />
+	<meta property="og:site_name" content="QuickD" />
+	<meta property="fb:admins" content="508608046" />
 
-        <!-- Styling -->
-        <link rel="stylesheet" href="<?= ROOT_URL ?>touch/resources/css/sencha-touch.css" type="text/css" /> 
-      	<link rel="stylesheet" href="<?= ROOT_URL ?>resources/css/example.css" type="text/css" />
-        
+	<!-- Styling -->
+	<link rel="stylesheet" href="<?= ROOT_URL ?>touch/resources/css/sencha-touch.css" type="text/css" /> 
+	<link rel="stylesheet" href="<?= ROOT_URL ?>resources/css/example.css" type="text/css" />
+</head>
+<body>
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="<?= LIBS_URL ?>jquery/jquery-1.7.min.js"><\/script>')</script>
+	<script src="<?= LIBS_URL ?>jquery/jquery.color.js"></script>
+	<script> 
+		var isDesktop = "<?= $isDesktop ?>";
+		var isIphone = "<?= $isIphone ?>";
+		var ROOT_URL = "<?= ROOT_URL ?>";
+		var IMG_URL = "<?= IMAGES_URL ?>";
+		var userbeta = "<?= $user ?>";
+		var sendmail = "<?= $sendmail ?>";
+	</script>
+	
+	<!-- Application -->
+	<script src="<?= ROOT_URL ?>app/function.js"></script> 
+	<script src="<?= ROOT_URL ?>app/libs/countdown.js"></script>
+	<script src="<?= ROOT_URL ?>app/libs/scroller-min.js"></script>
+	<script src="<?= ROOT_URL ?>app/libs/EasyScroller.js"></script>
+	<?php if($live): ?>
+	<script src="<?= ROOT_URL ?>touch/sencha-touch.js"></script>
+	<script src="<?= ROOT_URL ?>app-all.js"></script>
+	<? else: ?>
+	<script src="<?= ROOT_URL ?>touch/sencha-touch-debug.js"></script>
+	<script src="<?= ROOT_URL ?>app.js"></script>
+	<? endif; ?>
+	
+	<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
 
-        <!-- Live Reload -->
-        <!--<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
-        -->    
-    </head>
-    <body>
-    
-    
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?= LIBS_URL ?>jquery/jquery-1.7.min.js"><\/script>')</script>
-    <script src="<?= LIBS_URL ?>jquery/jquery.color.js"></script>
-    <script> 
-        var isDesktop = "<?= $isDesktop ?>";
-        var isIphone = "<?= $isIphone ?>";
-        var ROOT_URL = "<?= ROOT_URL ?>";
-        var IMG_URL = "<?= IMAGES_URL ?>";
-        var userbeta = "<?= $user ?>";
-        var sendmail = "<?= $sendmail ?>";
-    </script>
-    
-    <!-- Application -->
-    <script type="text/javascript" src="<?= ROOT_URL ?>app/function.js"></script> 
-    <script type="text/javascript" src="<?= ROOT_URL ?>app/libs/countdown.js"></script>
-    <script type="text/javascript" src="<?= ROOT_URL ?>app/libs/scroller-min.js"></script>
-    <script type="text/javascript" src="<?= ROOT_URL ?>app/libs/EasyScroller.js"></script>
-    <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-31044041-1']);
-        _gaq.push(['_setDomainName', 'quickd.com']);
-        _gaq.push(['_trackPageview']);
+	<script>
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-31044041-1']);
+		_gaq.push(['_setDomainName', 'quickd.com']);
+		_gaq.push(['_trackPageview']);
 
-        (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-    </script>
-    <?php if($live){ ?>
-        <script type="text/javascript" src="<?= ROOT_URL ?>touch/sencha-touch.js"></script>
-        <script type="text/javascript" src="<?= ROOT_URL ?>app-all.js"></script>
-    <? } else{ ?>
-        <script type="text/javascript" src="<?= ROOT_URL ?>touch/sencha-touch-debug.js"></script>
-        <script type="text/javascript" src="<?= ROOT_URL ?>app.js"></script>
-    <? } ?>
-
-        <div id="fb-root"></div>
-    </body>
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+</body>
 </html>
