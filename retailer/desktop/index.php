aff0b26f879427f684b037ba28261c0636c7d223
Kasper Pihl Tornøe <kasper@pihl.it>
2013-04-13 18:03:51 +0200

    More updates in retailer

--- retailer/desktop/index.php
+++ retailer/desktop/index.php
@@ -17,8 +17,6 @@ if(strpos($_SERVER['REQUEST_URI'], $thisfolder )){
 		
 	    <link href="<?= LIBS_URL ?>jquery/css/jquery.ui.theme.css" media="all" rel="stylesheet" type="text/css"/>
 	    <link href="<?= LIBS_URL ?>jquery/css/jquery.ui.selectmenu.css" media="all" rel="stylesheet"/>
-	    <link href="<?= LIBS_URL ?>jquery/css/jquery.ui.datepicker.css" media="all" rel="stylesheet"/>
-	        
 		<link href="<?= ROOT_URL ?>styles/stylesheets/screen.css" media="all" rel="stylesheet"/>
 	    <!--[if lt IE 9]>
 			  <link href="<?= ROOT_URL ?>styles/stylesheets/ie.css" media="all" rel="stylesheet"/>
